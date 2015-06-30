aa5941c7915579290fb6e924c597409a1b08a124
Sheldon Burks <sheldon.burks@gmail.com>
2015-06-30 16:40:07 -0400

    Initial template completed

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
+
+$(".glyphicon-menu-hamburger").hover(function() {
+  $(this).toggleClass("hover");
+},
+function() {
+  $(this).toggleClass("hover");
+});
